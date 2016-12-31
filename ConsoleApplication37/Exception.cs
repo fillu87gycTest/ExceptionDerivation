4b25a705721ea0e4acce4351508dc1314eeabaa3
fillu87gyc <wander.off65@gmail.com>
2017-01-01 04:50:53 +0900

    add

---
+++ ConsoleApplication37/Exception.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+class NonIntResult : Exception
+{
+	public NonIntResult() : base() { }
+	public NonIntResult(string message) : base(message) { }
+	public NonIntResult(string message, Exception inner) : base(message, inner) { }
+	protected NonIntResult(
+	      SerializationInfo info,
+	      StreamingContext context) : base(info, context) { }
+	public override string ToString()
+	{
+		return Message; 
+	}
+}
