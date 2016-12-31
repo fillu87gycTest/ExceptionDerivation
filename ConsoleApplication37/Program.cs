dc73d19659077c16bda5e80fdb628b7881dbe34c
fillu87gyc <wander.off65@gmail.com>
2017-01-01 04:49:36 +0900

    完成

--- ConsoleApplication37/Program.cs
+++ ConsoleApplication37/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication37
+class Program
 {
-	class Program
+	static void Main()
 	{
-		static void Main(string[] args)
+		int[] numb	= { 4, 8, 15, 32, 64, 127, 256, 512 };
+		int[] denom	= { 2, 0, 4, 4, 0, 8 };
+		for (int i = 0; i < numb.Length; i++)
 		{
+			try
+			{
+				if ((numb[i] % 2) != 0)				
+					throw new NonIntResult("Outcome of " + numb[i] + " / " + denom[i] + "is not even");
+				else Console.WriteLine(numb[i] + " / " + denom[i] + " is " + numb[i] / denom[i]);
+			}
+			catch (DivideByZeroException)
+			{
+				Console.WriteLine("dev by zero");
+			}
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine("No matching element found");
+			}
+			catch(NonIntResult exc)
+			{
+				Console.WriteLine(exc);
+			}
 		}
+		Console.ReadLine();
 	}
 }
